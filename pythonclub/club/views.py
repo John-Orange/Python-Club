6b478456312bf2f93287a51c98b8c1284123df3a
Zhongyuan Huang <zhongyuan.huang1996@gmail.com>
2022-02-09 13:24:09 -0800

    second Resources view commit

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
+def index(request):
+    return render(request, 'club/index.html')
+
+def Resources(request):
+    resources_list=Resource.objects.all()
+    return render(request, 'club/Resources.html',{'resources_list': resources_list})
